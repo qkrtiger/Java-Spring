4aac7db01a09d7eea2c0b020278fe307b4521b1b
PARK HO YEON <133315262+qkrtiger@users.noreply.github.com>
2023-05-28 21:45:41 +0900

    회원 관리
    서비스 코드 추가

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원은 거부
+        //커맨드 옵션 브이가 옵셔널 변환해줌..
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+//        memberRepository.save(member);
+//        return member.getId();
+
+        //같은 명령어
+        //이런 경우에는 메소드로 묶어주는 게 좋다(컨트롤 t)
+//        memberRepository.findByName(member.getName())
+//                .ifPresent(m -> {
+//                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+//                });
+//
+//        memberRepository.save(member);
+//        return member.getId();
+        validateDuplicateMember(member); //중복회원 검증용 메소드
+        //중복회원 검증 통과하면 회원 정보를 저장한다.
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
